80e5624863f72a1706eed372e29a9883bc6e2a76
Grupo-Vitta <matias.artesi@grupoattain.com.ar>
2024-11-04 15:53:57 -0300

    Update __manifest__.py

--- entregar_shipping 1/__manifest__.py
+++ entregar_shipping 1/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': 'Integración de métodos de envío con Entregar API',
     'description': 'Módulo personalizado para integrar los métodos de envío de Entregar en Odoo 17.',
     'author': 'Sebastián Díaz',
-    'depends': ['base', 'delivery'],
+    'depends': ['base', 'delivery', 'sale'],
     'data': [
         'views/entregar_shipping_views.xml',
         'security/ir.model.access.csv',
